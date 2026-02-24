0d8bdca189285b6fa98574508c7ecfccaa409b9e
Inam Lashari <lashariinam50@gmail.com>
2026-02-24 05:19:48 +0500

    add auth service

---
+++ Authentication-Service/src/main/java/com/example/Authentication/Servic/AuthenticationServicApplication.java
@@ -0,0 +1,13 @@
+package com.example.Authentication.Servic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationServicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationServicApplication.class, args);
+	}
+
+}
---
+++ Authentication-Service/src/test/java/com/example/Authentication/Servic/AuthenticationServicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Authentication.Servic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
