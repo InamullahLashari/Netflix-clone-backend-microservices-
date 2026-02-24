026554c624b80012e1db9c5ab232b4b6848dc538
Inam Lashari <lashariinam50@gmail.com>
2026-02-24 05:17:54 +0500

    add central service

---
+++ netflix-discovery-server/src/test/java/com/example/server/netflix_discovery_server/NetflixDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.server.netflix_discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
